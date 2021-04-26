de09d4d069fd0793c3eff31ed58015cf089b616a
Mrlquanin <65647293+Mrlquanin@users.noreply.github.com>
2021-04-26 19:51:43 +0800

    Add files via upload

---
+++ 第一次作业/第一次作业第一小题/第一次作业第一小题/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第一次作业
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 0;
+            Console.Write("请输入第一个数字");
+            string m = Console.ReadLine();
+            int a = Int32.Parse(m); 
+            Console.Write("在+ - * / 中选择输入你想用的运算符");
+            string s = Console.ReadLine();
+            Console.Write("请输入第二个数字");
+            string n = Console.ReadLine();
+            int b = Int32.Parse(n);
+            if (s == "+")
+            {
+                num = a + b;
+            }
+            if (s == "-")
+            {
+                num = a - b;   
+            }
+            if (s == "*")
+            {
+                num = a * b;
+            }
+            if (s == "/")
+            {
+                num = a / b;
+            }
+            string D = num.ToString();
+            Console.WriteLine("结果是"+num);
+
+
+
+
+
+        }
+    }
+}
---
+++ 第一次作业/第一次作业第二小题/第一次作业第二小题/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 第一次作业第二小题
+{
+    public partial class Form1 : Form
+    {
+        string s;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("*");
+            comboBox1.Items.Add("/");
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+
+            int a = Convert.ToInt32(textBox1.Text);
+
+       
+
+            int b = Convert.ToInt32(textBox2.Text);
+
+            if (s == "+")
+            {
+                num = a + b;
+            }
+            if (s == "-")
+            {
+                num = a - b;
+            }
+            if (s == "*")
+            {
+                num = a * b;
+            }
+            if (s == "/")
+            {
+                num = a / b;
+            }
+            string D = num.ToString();
+            textBox3.Text = D;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             s = comboBox1.Text;
+        }
+    }
+}
