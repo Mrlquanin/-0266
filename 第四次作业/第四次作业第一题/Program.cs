c3456fd77657cdea77c7d40f6936a5bd774a7956
Mrlquanin <65647293+Mrlquanin@users.noreply.github.com>
2021-04-29 21:23:15 +0800

    Add files via upload

---
+++ 第四次作业/第四次作业第一题/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace 第四次作业
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> n = head;
+            while (n != null)
+            {
+                action(n.Data);
+                n = n.Next;
+            }
+        }
+    }
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int sum = 0;
+           
+            intlist.ForEach(n => Console.Write($"{n},"));
+            intlist.ForEach(n => max = n > max ? n : max);
+            intlist.ForEach(n => min = n < min ? n : min);
+            intlist.ForEach(n => sum += n);
+            
+            Console.WriteLine("最大值："+max);
+            Console.WriteLine("最小值：" + min);
+            Console.WriteLine("和为：" + sum);
+        }
+    }
+}
---
+++ 第四次作业/第四次作业第二题/第四次作业第二题/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace 第四次作业第二题
+{
+
+
+    public delegate void AlarmHandler(object sender);
+    public delegate void TickHandler(object sender);
+
+    public class Clock
+    {
+        public event TickHandler Tick;
+        public event AlarmHandler Alarm;
+        public string AlarmTime { get; set; }
+        public string NowTime { get; set; }
+
+       
+     
+        public void tick(string time)
+        {
+            NowTime = time;
+            Tick(this);
+        }
+        public void alarm(string time)
+        {
+            if (NowTime == AlarmTime)
+            {
+                
+                Alarm(this);
+            }
+        }
+        public void SetAlarm()
+        {
+            Console.WriteLine("请设定你的闹铃时间：");
+            AlarmTime = Console.ReadLine();
+            
+        }
+
+        
+    }
+
+    class show
+    {
+        public Clock c = new Clock();
+        public show()
+        {
+            c.Alarm += new AlarmHandler(alarm);
+            c.Tick += new TickHandler(tick);
+        }
+
+
+        void alarm(object sender)
+        {
+
+            Console.WriteLine("叮叮叮！");
+            Console.WriteLine("叮叮叮！");
+            Console.WriteLine("叮叮叮！");
+
+        }
+        void tick(object seder)
+        {
+            Console.WriteLine("Tick! Nowtime:" + c.NowTime);
+        }
+
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Clock c = new Clock();
+            c.SetAlarm();
+
+            show s = new show();
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+               s.c.alarm(DateTime.Now.ToString("T"));
+               s.c.tick(DateTime.Now.ToString("T"));
+            }
+        }
+    }
+   
+}
