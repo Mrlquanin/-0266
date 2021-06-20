2c6c3743deb6379b9a8e24991bd8048c24c4daec
Mrlquanin <65647293+Mrlquanin@users.noreply.github.com>
2021-06-20 18:54:25 +0800

    Add files via upload

---
+++ 第九次作业/homework9/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace homework9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stbutton = new System.Windows.Forms.Button();
+            this.refreshbutton = new System.Windows.Forms.Button();
+            this.URLbindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.urlDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.oKDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.messageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.URLbindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 25);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "初始URL：";
+            // 
+            // stbutton
+            // 
+            this.stbutton.Location = new System.Drawing.Point(517, 56);
+            this.stbutton.Name = "stbutton";
+            this.stbutton.Size = new System.Drawing.Size(97, 32);
+            this.stbutton.TabIndex = 2;
+            this.stbutton.Text = "开始爬虫";
+            this.stbutton.UseVisualStyleBackColor = true;
+            this.stbutton.Click += new System.EventHandler(this.stbutton_Click);
+            // 
+            // refreshbutton
+            // 
+            this.refreshbutton.Location = new System.Drawing.Point(667, 56);
+            this.refreshbutton.Name = "refreshbutton";
+            this.refreshbutton.Size = new System.Drawing.Size(97, 32);
+            this.refreshbutton.TabIndex = 4;
+            this.refreshbutton.Text = "刷新";
+            this.refreshbutton.UseVisualStyleBackColor = true;
+            this.refreshbutton.Click += new System.EventHandler(this.refreshbutton_Click);
+            // 
+            // URLbindingSource
+            // 
+            this.URLbindingSource.DataSource = typeof(homework.Urls);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.urlDataGridViewTextBoxColumn,
+            this.oKDataGridViewCheckBoxColumn,
+            this.messageDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.URLbindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(798, 341);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // urlDataGridViewTextBoxColumn
+            // 
+            this.urlDataGridViewTextBoxColumn.DataPropertyName = "Url";
+            this.urlDataGridViewTextBoxColumn.HeaderText = "Url";
+            this.urlDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.urlDataGridViewTextBoxColumn.Name = "urlDataGridViewTextBoxColumn";
+            // 
+            // oKDataGridViewCheckBoxColumn
+            // 
+            this.oKDataGridViewCheckBoxColumn.DataPropertyName = "OK";
+            this.oKDataGridViewCheckBoxColumn.HeaderText = "OK";
+            this.oKDataGridViewCheckBoxColumn.MinimumWidth = 6;
+            this.oKDataGridViewCheckBoxColumn.Name = "oKDataGridViewCheckBoxColumn";
+            // 
+            // messageDataGridViewTextBoxColumn
+            // 
+            this.messageDataGridViewTextBoxColumn.DataPropertyName = "message";
+            this.messageDataGridViewTextBoxColumn.HeaderText = "message";
+            this.messageDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.messageDataGridViewTextBoxColumn.Name = "messageDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.refreshbutton);
+            this.Controls.Add(this.stbutton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.URLbindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button stbutton;
+        private System.Windows.Forms.Button refreshbutton;
+        private System.Windows.Forms.BindingSource URLbindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn urlDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn oKDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn messageDataGridViewTextBoxColumn;
+    }
+}
+
